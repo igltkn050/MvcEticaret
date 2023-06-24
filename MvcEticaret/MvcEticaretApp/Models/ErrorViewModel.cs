c7cc7cd42efc42425fd7d7c092de99843a9cf293
İdris Gültekin <bionicalsoftware@gmail.com>
2023-06-24 07:49:12 +0300

    Proje oluşturuldu.

---
+++ MvcEticaret/MvcEticaretApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcEticaretApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
