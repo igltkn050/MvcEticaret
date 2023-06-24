00e8013443b106b30eb74a3f59e9d91670e3d0c3
İdris Gültekin <bionicalsoftware@gmail.com>
2023-06-24 13:52:34 +0300

    Proje tamamlandı

---
+++ MvcEticaret/MvcEticaretApp/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcEticaretApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int StarCount { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsSale { get; set; }
+    }
+}
